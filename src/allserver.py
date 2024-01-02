6806e18d5a488aaeed517e5b291a823b6b3f8842
stsaria <solothunder.autoer@gmail.com>
2024-01-02 19:34:12 +0900

    hh

--- src/allserver.py
+++ src/allserver.py
@@ -7,14 +7,17 @@ elif not "src" in os.path.abspath(os.path.dirname(os.path.abspath(sys.argv[0])))
 else:
     os.chdir(os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), '..')))
 
-import check, gui
+import build, check, gui
 
-def main():
+def main(args : list):
     result = check.check()
     if result != 0:
         return 1
-    
-    gui.gui_start()
+    if len(args) >= 2:
+        if "--build" in args:
+            build.install()
+    else:
+        gui.gui_start()
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
